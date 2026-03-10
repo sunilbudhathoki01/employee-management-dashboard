d370436538c6c1ade02f755ef6b372b05f17655f
sunilbudhathoki01 <ashishbudhathoki952@gmail.com>
2026-03-10 20:17:12 +0545

    feat:create dashboard and employee pages

---
+++ src/pages/EmployeeDetails.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EmployeeDetails = () => {
+  return (
+    <div>
+      <h4>employeeDetails</h4>
+    </div>
+  );
+};
+
+export default EmployeeDetails;
---
+++ src/pages/Employees.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Employees = () => {
+  return (
+    <div>
+      <h2>employee</h2>
+    </div>
+  );
+};
+
+export default Employees;
